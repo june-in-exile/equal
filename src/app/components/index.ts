0dd736bef9c3319834ac79a30b315688ba230169
June <df41022@gmail.com>
2024-10-21 20:29:54 +0800

    handleMetamaskVerify success

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Verify } from './verify';
 export { default as Read } from './read';
 export { default as Status } from './status';
-export { default as Metamask } from './metamask';
 export { default as Description } from './description';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from 'react';
-import { Verify, Read, Status, Description, Metamask } from './components';
+import { Verify, Read, Status, Description } from './components';
 import { VerificationState } from './type';
 import dotenv from 'dotenv';
 
@@ -12,7 +12,6 @@ export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start">
-                <Metamask />
                 <div className="flex gap-6 items-center flex-col sm:flex-row">
                     <Verify verification={verification} setVerification={setVerification} />
                     <Read />
